ee01f2b31fee4cc58c229bcbef0ecd94beed3a07
geomathe <u2008032@rajagiri.edu.in>
2024-11-21 13:35:11 +0530

    added var v4 to branch u2

--- add.py
+++ add.py
@@ -7,5 +7,5 @@ Original file is located at
     https://colab.research.google.com/drive/1zbz7f1U5qW3qRg9u_boBMz8jwsg6ffVS
 """
 
-def add(n1,n2,n3):
-    print(n1+n2+n3)
+def add(n1,n2,n3,n4):
+    print(n1+n2+n3+n4)
